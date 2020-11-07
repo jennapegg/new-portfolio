b5a5d990468be61c595d6448c17d3a24ea1c9b11
Jenna Pegg <jenna.pegg.vic@gmail.com>
2020-11-07 12:59:53 -0800

    remove unused footer functions

--- themes/portfolio/footer.php
+++ themes/portfolio/footer.php
@@ -9,16 +9,10 @@
  */
 ?>
 
-<footer class="footer-container">
-	<div class="footer-grid">
-		<?php dynamic_sidebar( 'footer-widgets' ); ?>
-	</div>
-</footer>
-
-<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
-	</div><!-- Close off-canvas content -->
-<?php endif; ?>
-
-<?php wp_footer(); ?>
-</body>
+		<footer class="footer-container">
+			<div class="footer-grid">
+			</div>
+		</footer>
+		<?php wp_footer(); ?>
+	</body>
 </html>
